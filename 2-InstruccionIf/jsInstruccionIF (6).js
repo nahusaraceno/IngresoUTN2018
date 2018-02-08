c14f554b38712a9de17d6050a04c7eaa6b8f90e6
nahusaraceno <36230628+nahusaraceno@users.noreply.github.com>
2018-02-08 12:42:24 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,7 +1,17 @@
-function Mostrar()
-{
-//tomo la edad  
+function Mostrar() {
+    //tomo la edad  
+    var edad;
+    edad = document.getElementById("edad").value;
+  if(edad < 13){
+      alert("niño");
+  }else if(edad < 18){
+      alert("adolescsnte");
+  }
+  else{
+      alert("adulto");
+  }
 
+  
 
 
 
