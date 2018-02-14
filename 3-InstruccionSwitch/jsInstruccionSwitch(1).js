b8d105d7eb2f87440d22743662f2499607ca2e47
nahusaraceno <36230628+nahusaraceno@users.noreply.github.com>
2018-02-14 12:31:28 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(1).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(1).js
@@ -3,6 +3,20 @@ function Mostrar()
 //tomo el mes
 var mesDelAño = document.getElementById('mes').value;
 
+switch(mesDelAño)
+{
+    case "Enero":alert("Que comiences bien el año") 
+    break;
+    case "Marzo":alert("A clases") 
+    break;
+    case "Julio":alert("Se vienen las vacaciones") 
+    break;
+    case "Diciembre":alert("Felices fiestas") 
+    break;
+
+
+
+}
 
 
 
--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -2,10 +2,34 @@ function Mostrar()
 {
 //tomo el mes 
 var mesDelAño = document.getElementById('mes').value;
+switch (mesDelAño)
+{
+case "Enero":
+      
+    case "Febrero":
+    
+    case "Marzo":
+       
+    case "Abril":
+      
+    case "Mayo":
+      
+    case "Junio":alert("Todavia no llego el invierno")
+    break;    
+    case "Julio":
+     
+    case "Agosto":alert("Estamos en invierno")
+    break;  
+    case "Septiembre":
+    
+    case "Octubre":
+    
+    case "Noviembre":
+    
+    case "Diciembre":alert("Ya paso el invierno")
+    break;
 
-alert (mesDelAño);
 
-
-
+}
 
 }//FIN DE LA FUNCIÓN
